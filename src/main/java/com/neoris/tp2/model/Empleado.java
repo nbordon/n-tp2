aa5e03a12e285e80e5cb086a115f95005603ba4b
Pedro Nicolas Bordon <nbordon@frba.utn.edu.ar>
2022-06-15 21:48:25 -0300

    refactor con anotations de lombok

--- src/main/java/com/neoris/tp2/model/Empleado.java
+++ src/main/java/com/neoris/tp2/model/Empleado.java
@@ -1,48 +1,23 @@
 package com.neoris.tp2.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Empleado {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String nombre;
-    private String apellido;
-
-    public Empleado(){
-
-    }
-
-    public Empleado(Integer id, String nombre) {
-        this.id = id;
-        this.nombre = nombre;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
+    @Setter private String nombre;
+    @Setter private String apellido;
 }
